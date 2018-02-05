35fc06cbf57086762f9dad38a281953eee4c9ee0
James Leasure <james.leasure@tri-c.edu>
2018-02-05 12:48:45 -0500

    InClass02

---
+++ InClass02/myScript.js
@@ -0,0 +1,12 @@
+function myFunction() {
+    document.getElementById("demo").innerHTML = "Changed by Function.";
+}
+
+function addNumber() {
+    
+}
+
+function tryEscapeSequences() {
+    document.getElementById("demo").innerHTML = "It's a great day!";
+
+}
