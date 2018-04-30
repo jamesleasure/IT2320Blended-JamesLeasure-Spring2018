6626a4bba6fa75476c0632121268519aaa2cbbc6
James Leasure <james.leasure@tri-c.edu>
2018-04-30 11:36:49 -0400

    Modified Lab8 Starting file

---
+++ ReactJS1/app.js
@@ -0,0 +1,17 @@
+//import React from 'react';
+//import ReactDOM from 'react-dom';
+
+// Write code here:
+const toDoList = (
+  <ol>
+    <li>Learn React</li>
+    <li>Become a Developer</li>
+    <li>Become a hacker</li>
+    <li>Create a better Facebook</li>
+  </ol>
+);
+
+ReactDOM.render(
+  toDoList, 
+  document.getElementById('app')
+);
